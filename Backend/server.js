c05701e2d867de8311390ec47ec88ef26307e884
krushna <krishnagate89@gmail.com>
2025-11-13 17:32:23 +0530

    try to solve vercel error

--- Backend/server.js
+++ Backend/server.js
@@ -18,8 +18,8 @@ const app = express();
 // );
 const allowedOrigins = [
   "http://localhost:5173",
-  // "https://taskmangerhously.vercel.app",
-  "http://localhost:5173",
+  "https://taskmangerhously.vercel.app",
+  // "http://localhost:5173",
 ];
 
 app.use(
