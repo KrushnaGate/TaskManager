2492e270d26e3309f31ceaced93d850da6eb12fe
krushna <krishnagate89@gmail.com>
2025-11-13 08:28:46 +0530

    added vercel.json file

--- Frontend/task/vite.config.js
+++ Frontend/task/vite.config.js
@@ -5,4 +5,15 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // server: {
+  //   port: 5000,
+  // },
+  // build: {
+  //   outDir: "dist",
+  //   sourcemap: false,
+  // },
+  // // Add this for Vercel deployment
+  // define: {
+  //   "process.env": {},
+  // },
 });
